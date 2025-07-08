1b707bcf3423b865c565be453cf178fe57522435
macclintx <macclintx@gmail.com>
2025-07-09 01:47:04 +0300

    function declarations

--- includes/functions.php
+++ includes/functions.php
@@ -5,9 +5,28 @@
     function filterByCategory($category, $array){
 
         $filter = array_filter($array, function($cat) use($category){
+            
             return $cat['category'] == $category;
         });
 
-        return $filter;
+        if ($category === 'all'){
+            return $array;
+        }else{
+            return $filter;
+        }
+        
         
     }
+
+
+
+    function addToCart($id, $quantity, &$array){
+
+        $new_stockQuantity = $array[$id - 1]['stock_quantity'] - $quantity;
+
+        $array[$id - 1]['stock_quantity'] = $new_stockQuantity;
+        
+        return $array[$id - 1];
+    }
+
+    
