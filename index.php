0e2e84f0fc8cb658fc05982c407ea8e07fc02f28
macclintx <macclintx@gmail.com>
2025-07-09 01:50:38 +0300

    web entry point

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
 session_start();
-    require __DIR__ . '/view/partials/header.php';
-    require_once 'includes/config.php';
-    require_once 'includes/functions.php';
+    require __DIR__. '/view/partials/header.php';
+    require __DIR__ . '/view/partials/footer.php';
+    include_once 'includes/config.php';
+    include_once 'includes/functions.php';
    
 
     //load initial data from config
@@ -14,27 +15,37 @@ session_start();
     $errors = [];
 
     $filt = [];
+    $cart = [];
 
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         
         //handle filter
         $filt[] = filterByCategory($_POST['category'], $products);
 
+        //cart variables
+        if(filter_has_var(INPUT_POST, 'quantity')){
+
+
+            $cart_id = filter_input(INPUT_POST, 'id',FILTER_VALIDATE_INT);
+            $cart_quantity = filter_input(INPUT_POST, 'quantity', FILTER_VALIDATE_INT);
             
-       
+            
+            $cart[] = [addToCart($cart_id, $cart_quantity, $products)];
+            $filt[] = $products;
+        }
+        
 
-
-      /*  if(filter_has_var(INPUT_POST, $_POST['category'])){
-            $filter = $_POST['category'];
-            $filtered[] = filterByCategory($filter, $products);
-            echo $filtered;
-        }*/  
+      
+    }else{
+        $filt[] = $products; 
     }
- 
+    
+    
 
     //handle filter
    // $filter = $_POST['category'];
     //$filtered = filterByCategory($filter ,$products);
 
  include 'view/inventory.php';
-require __DIR__ . '/view/partials/footer.php';
+
+ print_r($cart);
