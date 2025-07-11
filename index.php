bff85d22d48e75390cb34507705d0860b39d5bc6
macclintx <macclintx@gmail.com>
2025-07-12 02:33:49 +0300

    added cart to session to store items

--- index.php
+++ index.php
@@ -1,51 +1,45 @@
 <?php
 
 session_start();
-    require __DIR__. '/view/partials/header.php';
-    require __DIR__ . '/view/partials/footer.php';
-    include_once 'includes/config.php';
-    include_once 'includes/functions.php';
-   
 
-    //load initial data from config
-    if (!isset($_SESSION['products'])){
-        $_SESSION['products'] = $products;
-    }
+require __DIR__. '/view/partials/header.php';
+require __DIR__ . '/view/partials/footer.php';
+include_once 'includes/config.php';
+include_once 'includes/functions.php';
 
-    $errors = [];
+if (!isset($_SESSION['products'])){
+    $_SESSION['products'] = $products;
+}
 
-    $filt = [];
-    $cart = [];
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = [];
+}
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        
-        //handle filter
-        $filt[] = filterByCategory($_POST['category'], $products);
+$errors = [];
+$filt = [];
 
-        //cart variables
-        if(filter_has_var(INPUT_POST, 'quantity')){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Filter
+    $filt[] = filterByCategory($_POST['category'], $products);
 
+    // Add to cart
+    if (filter_has_var(INPUT_POST, 'quantity')) {
+        $cart_id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+        $cart_quantity = filter_input(INPUT_POST, 'quantity', FILTER_VALIDATE_INT);
 
-            $cart_id = filter_input(INPUT_POST, 'id',FILTER_VALIDATE_INT);
-            $cart_quantity = filter_input(INPUT_POST, 'quantity', FILTER_VALIDATE_INT);
-            
-            
-            $cart[] = [addToCart($cart_id, $cart_quantity, $products)];
-            $filt[] = $products;
+        $item = addToCart($cart_id, $cart_quantity, $products);
+
+        if ($item) {
+            $_SESSION['cart'][] = $item;
         }
-        
 
-      
-    }else{
-        $filt[] = $products; 
+        $filt[] = $products;
     }
-    
-    
 
-    //handle filter
-   // $filter = $_POST['category'];
-    //$filtered = filterByCategory($filter ,$products);
+} else {
+    $filt[] = $products;
+}
 
- include 'view/inventory.php';
+include 'view/inventory.php';
 
- print_r($cart);
+print_r($_SESSION['cart']);
